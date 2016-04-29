8d99037eb9be0d42d73ef1ce88659c85649ceef9
Nathaniel J. Smith <njs@pobox.com>
2016-04-29 02:06:59 -0700

    clean up some defunct code/comments

--- h11/util.py
+++ h11/util.py
@@ -1,24 +1,12 @@
 __all__ = ["bytesify"]
 
-# Strategy: most of our error-checking code is used both to catch usage errors
-# by whoever's using this library, and also to catch naughtiness on behalf of
-# remote hosts. But it's useful to be able to distinguish these. So,
-# internally we raise ProtocolError whenever something goes wrong. And then
-# when processing data from the remote host, we catch these and convert them
-# to RemoteProtocolError. So from the user point of view, ProtocolError -> you
-# screwed up, RemoteProtocolError -> someone else screwed up, maybe you should
-# send a 400 Invalid Request or something like that. (If possible. I mean,
-# obviously not if you're a client, or the error was "connection closed
-# unexpectedly" or whatever.)
+# This indicates either that you tried to do something that HTTP/1.1 says is
+# illegal, or that your peer did. Either way, you should probably close the
+# connection and think things over.
 class ProtocolError(Exception):
     def __init__(self, msg, error_status_hint=400):
         Exception.__init__(self, msg)
         self.error_status_hint = error_status_hint
-
-class RemoteProtocolError(Exception):
-    def __init__(self, base):
-        Exception.__init__(*base.args)
-        self.error_status_hint = base.error_status_hint
 
 def validate(regex, data, msg="malformed data"):
     match = regex.match(data)
