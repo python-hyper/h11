c1c49b0e1718331663ee109f3417aff97fd23b70
Nathaniel J. Smith <njs@pobox.com>
2016-05-07 03:02:30 -0700

    Add minimal setup.py for RTD

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Minimal setup.py to get readthedocs working, not recommended for real use
+
+from distutils.core import setup
+
+setup(name="h11",
+      version="0.0.0",
+      packages=["h11"],
+      )
