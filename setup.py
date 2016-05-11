a2ac088d0df55a632e54e0a36ee2cad18238f5f3
Nathaniel J. Smith <njs@pobox.com>
2016-05-10 23:31:01 -0700

    Add real setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,29 @@
-# Minimal setup.py to get readthedocs working, not recommended for real use
+import sys
+from setuptools import setup
 
-from distutils.core import setup
+# defines __version__
+exec(open("h11/_version.py").read())
 
-setup(name="h11",
-      version="0.0.0",
-      packages=["h11"],
-      )
+setup(
+    name="h11",
+    version=__version__,
+    description=
+        "A pure-Python, bring-your-own-I/O implementation of HTTP/1.1",
+    long_description=open("README.rst").read(),
+    author="Nathaniel J. Smith",
+    author_email="njs@pobox.com",
+    license="MIT",
+    packages=["h11"],
+    url="https://github.com/njsmith/h11",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: System :: Networking",
+        ],
+)
