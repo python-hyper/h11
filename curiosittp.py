eeb5a427452652b6b04d71d8f7c31370cd6cfc0f
Nathaniel J. Smith <njs@pobox.com>
2016-05-15 15:42:29 -0700

    talking to myself

--- curiosittp.py
+++ curiosittp.py
@@ -1,5 +1,91 @@
 # WIP
 
+# handlers are async functions (duh)
+# maybe they can throw werkzeug errors?
+# do they get explicit access to the h11 stack?
+# - how to handle upgrades?
+#
+# for normal day-to-day use want stuff like:
+# - get method, target
+#   - handle target weirdnesses? I guess router has to do some of that
+#   - handle query string
+# - get the request headers and async iterate over the body
+# - await send response headers
+# - await send body chunk
+# - await done()  (+ trailing headers)
+# and convenience stuff:
+# - parse various request headers
+#   - werkzeug.parse_accept_header, etc
+# - grab body as JSON (after checking content-type and with length limit)
+#   - maybe form parsing as well
+# - set up response headers in slightly more ergonomic way than raw list
+# - throw an error to give a standard response page
+#
+# - if handling routing through werkzeug, probably need to handle the routing
+#   errors, so I guess that's a basic WSGI thingummy for me...
+#   - actually the werkzeug HTTPException's look pretty easy to work with --
+#     just inspect ex.code, ex.get_body(), ex.get_headers()
+#     latter two take environ, get_body never uses it
+#     get_headers... also never uses it. okay!
+#   - RequestRedirect doesn't actually work right directly, need to either
+#     catch it explicitly and check .new_url, or else call .get_response()
+#   - oh, but .get_response() also works and accepts environ=None
+
+# XX FIXME: server loop needs to be able to distinguish between local and
+# remote ProtocolErrors
+# XX FIXME: I think receive_data(None) should be a different call
+#   receive_after_unpause()
+# XX FIXME: could h11 normalize url targets?
+# - eh. only proxies receive them in real life, they contain a bit more
+#   information than just the Host: header, there are some complications
+#   around validating them and the different situations they occur (CONNECT,
+#   OPTIONS, etc.)
+
+# We need some mechanism for generic code across multiple handlers --
+# e.g. adding CORS headers to a bunch of
+# maybe our API should allow for @decorators that can add headers? or complete
+# middleware?
+# maybe each handler should be an async generator that yields h11 events
+
+# headers as dict-of-lists:
+# - getone
+# - getall
+#   - or else say that everything but Set-Cookie is a "" and it's a [],
+#     unconditionally, and just use __getitem__?
+#     and server doesn't even get Set-Cookie
+#     maybe on the server we just represent headers as a dict... (Cookie: also
+#     can't be combined via ",", but RFC 6265 has a MUST NOT on sending
+#     multiple Cookie: headers, so that's how that's okay, I guess)
+#     or really a CIDict, maybe immutable
+# - del, assignment
+# - parsing for specific fields, like get the entries in a comma list instead
+#   of getting the raw headers
+#   - needs lots of per-header info
+
+# werkzeug header value parsers want strings, not bytes
+# http://werkzeug.pocoo.org/docs/0.11/unicode/
+# I think for us the right thing is to encode/decode method/target/headers as
+# ("ascii", "surrogateescape")
+# this requires 3.1 so it doesn't work for h11 itself, but whatever
+
+class RequestHeaders(collections.abc.Mapping):
+    def __init__(self, h11_headers):
+        dupes = {}
+        for (key, value) in h11_headers:
+            dupes.setdefault(key, []).append(value)
+        self._d = {}
+        for (key, values) in dupes.items():
+            self._d[key] = b", ".join(values)
+
+    def __getitem__(self, key):
+        return self._d[asciify(key).lower()]
+
+    def __iter__(self):
+        return iter(self._d)
+
+    def __len__(self):
+        return len(self._d)
+
 # Curio issues:
 # - missing socket.shutdown
 # - maybe a context manager for settimeout? maybe settimeout itself could be a
