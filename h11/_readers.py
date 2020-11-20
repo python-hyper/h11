3247e285d53092bb474f9db81e3df807e040cd82
cdeler <serj.krotov@gmail.com>
2020-11-20 20:52:15 +0300

    Changed the maybe_extract_lines logic according PR review
    1. it uses b"\n\r?\n" as a blank line delimiter regex
    2. it splits lines using b"\r?\n" regex, so that it's tolerant for mixed line endings
    3. for chunked encoding it rewind buffer until b"\r\n"
    
    The changes are based on this comment: https://github.com/python-hyper/h11/pull/115#issuecomment-731139500

--- h11/_readers.py
+++ h11/_readers.py
@@ -31,6 +31,7 @@ header_field_re = re.compile(header_field.encode("ascii"))
 # Remember that this has to run in O(n) time -- so e.g. the bytearray cast is
 # critical.
 obs_fold_re = re.compile(br"[ \t]+")
+strict_line_delimiter_regex = re.compile(b"\r\n", re.MULTILINE)
 
 
 def _obsolete_line_fold(lines):
@@ -154,7 +155,7 @@ class ChunkedReader(object):
         assert self._bytes_to_discard == 0
         if self._bytes_in_chunk == 0:
             # We need to refill our chunk count
-            chunk_header = buf.maybe_extract_until_next(line_delimiter_regex, 2)
+            chunk_header = buf.maybe_extract_until_next(strict_line_delimiter_regex, 2)
             if chunk_header is None:
                 return None
             matches = validate(
