077b3253b727b60a16548008894ff61ac0e9c7ec
Michael Adkins <michael@prefect.io>
2022-03-28 10:48:16 -0500

    Improve bytearray cast comment

--- h11/_readers.py
+++ h11/_readers.py
@@ -37,9 +37,6 @@ from ._util import LocalProtocolError, RemoteProtocolError, Sentinel, validate
 __all__ = ["READERS"]
 
 header_field_re = re.compile(header_field.encode("ascii"))
-
-# Remember that this has to run in O(n) time -- so e.g. the bytearray cast is
-# critical.
 obs_fold_re = re.compile(br"[ \t]+")
 
 
@@ -52,6 +49,7 @@ def _obsolete_line_fold(lines: Iterable[bytes]) -> Iterable[bytes]:
             if last is None:
                 raise LocalProtocolError("continuation line at start of headers")
             if not isinstance(last, bytearray):
+                # Cast to a mutable type, avoiding copy on append to ensure O(n) time
                 last = bytearray(last)
             last += b" "
             last += line[match.end() :]
